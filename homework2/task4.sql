438982f2e310b0cdc5558e1dab4bb2c79b709930
M01eg <ggff6665@bk.ru>
2019-11-08 08:45:18 +0300

    Create task4.sql

---
+++ homework2/task4.sql
@@ -0,0 +1,16 @@
+/*
+ Урок 2
+ Задание 4
+
+(по желанию) Ознакомьтесь более подробно с документацией утилиты mysqldump.
+Создайте дамп единственной таблицы help_keyword базы данных mysql.
+Причем добейтесь того, чтобы дамп содержал только первые 100 строк таблицы.
+*/
+
+
+/*
+
+Делаем дамп:
+C:\Users\User\AppData\Roaming\DBeaverData\workspace6\General>"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" -u root --where="true limit 100" -p mysql help_keyword >second_dump.sql
+
+*/
