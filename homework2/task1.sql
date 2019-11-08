f6e3378cbcd2c0a1c1a6b1376617b37ed3904415
M01eg <ggff6665@bk.ru>
2019-11-08 07:59:50 +0300

    Update task1.sql

--- homework2/task1.sql
+++ homework2/task1.sql
@@ -16,6 +16,6 @@ https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-community-8.0
 
 
 "Создайте в домашней директории файл .my.cnf..." - загрузил my.cnf как отдельный файл в репозитории:
-https://github.com/M01eg/gb_mysql/new/master/my.cnf
+https://github.com/M01eg/gb_mysql/blob/master/homework2/my.cnf
 
 */
