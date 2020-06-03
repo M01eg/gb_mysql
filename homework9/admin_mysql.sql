b979dd83997fbe461a7deab9a0d08290fb8acafe
M01eg <ggff6665@bk.ru>
2020-06-03 12:11:36 +0300

    Update admin_mysql.sql

--- homework9/admin_mysql.sql
+++ homework9/admin_mysql.sql
@@ -1,1 +1,20 @@
+/*
+ Урок 9
+ Задание 1
+Создайте двух пользователей которые имеют доступ к базе данных shop.
+Первому пользователю shop_read должны быть доступны только запросы
+на чтение данных, второму пользователю shop — любые операции в
+пределах базы данных shop.
+*/
 
+mysql> CREATE USER shop_read IDENTIFIED WITH sha256_password BY '1111';
+Query OK, 0 rows affected (0.06 sec)
+
+mysql> CREATE USER shop IDENTIFIED WITH sha256_password BY '1234';
+Query OK, 0 rows affected (0.11 sec)
+
+mysql> GRANT SELECT ON shop.* TO shop_read;
+Query OK, 0 rows affected (0.08 sec)
+
+mysql> GRANT ALL ON shop.* TO shop;
+Query OK, 0 rows affected (0.11 sec)
